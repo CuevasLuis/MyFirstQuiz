0d287bc84ae7e135b05197e656949cee3d98d642
CuevasLuis <luisc_92@yahoo.com>
2021-03-27 19:47:38 -0500

    added all files , js, css, html

---
+++ assests/questions.js
@@ -0,0 +1,41 @@
+
+var questions = [
+        {
+        title:"What is the closest planet to the sun in our solar System?",
+        choices: ["Pluto","Mars","Mercury","Earth",],
+        answer: "Mercury"
+        },
+    
+        {
+       title: "How many planets are in the milky way?",
+        choices:["nine ","eight","seven","twenty-one"],
+        answer: "nine"
+
+    
+        },  
+
+        {
+            title: "If you were born five years ago. how old are you right now?",
+            choices: ["five", "twenty-one", "seven", "ten"],
+            answer: "five"
+          },
+
+
+          { 
+            title: "How many feet in an inch",
+            choices: ["zero", "twelve", "two", "one"],
+            answer: "zero"
+          },
+
+
+          {
+            title: "Which one is not a continent",
+            choices: ["Mexico", "Africa", "Australia", "Europe"],
+            answer: "Mexico"
+          
+          },
+
+];
+
+
+
